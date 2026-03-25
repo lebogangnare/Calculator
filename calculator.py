4e057782a0ad4862aa96e232ce919509a51314a0
Ntsako <Ngobeni2302@gmail.com>
2026-03-25 22:57:59 +0200

    created the main file and put down the format/ structure

---
+++ calculator.py
@@ -0,0 +1,15 @@
+import tkinter
+
+button_values = [
+    ["clear", "+/-", "%", "÷"], 
+    ["7", "8", "9", "×"], 
+    ["4", "5", "6", "-"],
+    ["1", "2", "3", "+"],
+    ["0", ".", "√", "="]
+]
+
+right_symbols = ["÷", "×", "-", "+", "="]
+top_symbols = ["AC", "+/-", "%"]
+
+
+#for the numbers, find a way to minimize them instead of expanding or making the move further and further 
